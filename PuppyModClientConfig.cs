c1370ec82f804e1a813c14baad52a51aa34bf695
POLA <pola@puppymod.local>
2026-08-30 22:31:04 -0300

    feat(config): split starting puppy into server Enable and client Start, server wins

---
+++ PuppyModClientConfig.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace PuppyMod;
+
+public class PuppyModClientConfig : ModConfig
+{
+    public override ConfigScope Mode => ConfigScope.ClientSide;
+
+    [Header("PuppySet")]
+    [DefaultValue(true)]
+    public bool StartAsPuppy;
+}
